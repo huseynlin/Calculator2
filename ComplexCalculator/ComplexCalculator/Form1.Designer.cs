d9e1eff1429c97a767524a12d7c8eca47a8baf2b
huseynlin <nuralihuseynli53@gmail.com>
2025-10-07 01:02:23 +0400

    Calculator

---
+++ ComplexCalculator/ComplexCalculator/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace ComplexCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            richTextBox1 = new RichTextBox();
+            r1 = new Button();
+            r2 = new Button();
+            r3 = new Button();
+            cem = new Button();
+            ferq = new Button();
+            r6 = new Button();
+            r5 = new Button();
+            r4 = new Button();
+            hasil = new Button();
+            r9 = new Button();
+            r8 = new Button();
+            r7 = new Button();
+            nisbet = new Button();
+            beraber = new Button();
+            r0 = new Button();
+            nokta = new Button();
+            faiz = new Button();
+            sqrt = new Button();
+            c = new Button();
+            ce = new Button();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(281, 100);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "0";
+            // 
+            // r1
+            // 
+            r1.Location = new Point(11, 203);
+            r1.Name = "r1";
+            r1.Size = new Size(55, 45);
+            r1.TabIndex = 1;
+            r1.Text = "1";
+            r1.UseVisualStyleBackColor = true;
+            r1.Click += r1_Click;
+            // 
+            // r2
+            // 
+            r2.Location = new Point(85, 203);
+            r2.Name = "r2";
+            r2.Size = new Size(55, 45);
+            r2.TabIndex = 2;
+            r2.Text = "2";
+            r2.UseVisualStyleBackColor = true;
+            r2.Click += r2_Click;
+            // 
+            // r3
+            // 
+            r3.Location = new Point(160, 203);
+            r3.Name = "r3";
+            r3.Size = new Size(55, 45);
+            r3.TabIndex = 3;
+            r3.Text = "3";
+            r3.UseVisualStyleBackColor = true;
+            r3.Click += r3_Click;
+            // 
+            // cem
+            // 
+            cem.Location = new Point(237, 203);
+            cem.Name = "cem";
+            cem.Size = new Size(55, 45);
+            cem.TabIndex = 4;
+            cem.Text = "+";
+            cem.UseVisualStyleBackColor = true;
+            cem.Click += cem_Click;
+            // 
+            // ferq
+            // 
+            ferq.Location = new Point(238, 277);
+            ferq.Name = "ferq";
+            ferq.Size = new Size(55, 45);
+            ferq.TabIndex = 8;
+            ferq.Text = "-";
+            ferq.UseVisualStyleBackColor = true;
+            ferq.Click += ferq_Click;
+            // 
+            // r6
+            // 
+            r6.Location = new Point(161, 277);
+            r6.Name = "r6";
+            r6.Size = new Size(55, 45);
+            r6.TabIndex = 7;
+            r6.Text = "6";
+            r6.UseVisualStyleBackColor = true;
+            r6.Click += r6_Click;
+            // 
+            // r5
+            // 
+            r5.Location = new Point(86, 277);
+            r5.Name = "r5";
+            r5.Size = new Size(55, 45);
+            r5.TabIndex = 6;
+            r5.Text = "5";
+            r5.UseVisualStyleBackColor = true;
+            r5.Click += r5_Click;
+            // 
+            // r4
+            // 
+            r4.Location = new Point(12, 277);
+            r4.Name = "r4";
+            r4.Size = new Size(55, 45);
+            r4.TabIndex = 5;
+            r4.Text = "4";
+            r4.UseVisualStyleBackColor = true;
+            r4.Click += r4_Click;
+            // 
+            // hasil
+            // 
+            hasil.Location = new Point(237, 352);
+            hasil.Name = "hasil";
+            hasil.Size = new Size(55, 45);
+            hasil.TabIndex = 12;
+            hasil.Text = "x";
+            hasil.UseVisualStyleBackColor = true;
+            hasil.Click += hasil_Click;
+            // 
+            // r9
+            // 
+            r9.Location = new Point(160, 352);
+            r9.Name = "r9";
+            r9.Size = new Size(55, 45);
+            r9.TabIndex = 11;
+            r9.Text = "9";
+            r9.UseVisualStyleBackColor = true;
+            r9.Click += r9_Click;
+            // 
+            // r8
+            // 
+            r8.Location = new Point(85, 352);
+            r8.Name = "r8";
+            r8.Size = new Size(55, 45);
+            r8.TabIndex = 10;
+            r8.Text = "8";
+            r8.UseVisualStyleBackColor = true;
+            r8.Click += r8_Click;
+            // 
+            // r7
+            // 
+            r7.Location = new Point(11, 352);
+            r7.Name = "r7";
+            r7.Size = new Size(55, 45);
+            r7.TabIndex = 9;
+            r7.Text = "7";
+            r7.UseVisualStyleBackColor = true;
+            r7.Click += r7_Click;
+            // 
+            // nisbet
+            // 
+            nisbet.Location = new Point(237, 429);
+            nisbet.Name = "nisbet";
+            nisbet.Size = new Size(55, 45);
+            nisbet.TabIndex = 16;
+            nisbet.Text = "/";
+            nisbet.UseVisualStyleBackColor = true;
+            nisbet.Click += nisbet_Click;
+            // 
+            // beraber
+            // 
+            beraber.Location = new Point(160, 429);
+            beraber.Name = "beraber";
+            beraber.Size = new Size(55, 45);
+            beraber.TabIndex = 15;
+            beraber.Text = "=";
+            beraber.UseVisualStyleBackColor = true;
+            beraber.Click += beraber_Click;
+            // 
+            // r0
+            // 
+            r0.Location = new Point(85, 429);
+            r0.Name = "r0";
+            r0.Size = new Size(55, 45);
+            r0.TabIndex = 14;
+            r0.Text = "0";
+            r0.UseVisualStyleBackColor = true;
+            r0.Click += r0_Click;
+            // 
+            // nokta
+            // 
+            nokta.Location = new Point(11, 429);
+            nokta.Name = "nokta";
+            nokta.Size = new Size(55, 45);
+            nokta.TabIndex = 13;
+            nokta.Text = ".";
+            nokta.UseVisualStyleBackColor = true;
+            nokta.Click += nokta_Click;
+            // 
+            // faiz
+            // 
+            faiz.Location = new Point(238, 137);
+            faiz.Name = "faiz";
+            faiz.Size = new Size(55, 45);
+            faiz.TabIndex = 24;
+            faiz.Text = "%";
+            faiz.UseVisualStyleBackColor = true;
+            faiz.Click += faiz_Click;
+            // 
+            // sqrt
+            // 
+            sqrt.Location = new Point(161, 137);
+            sqrt.Name = "sqrt";
+            sqrt.Size = new Size(55, 45);
+            sqrt.TabIndex = 23;
+            sqrt.Text = "Sqrt";
+            sqrt.UseVisualStyleBackColor = true;
+            sqrt.Click += sqrt_Click;
+            // 
+            // c
+            // 
+            c.Location = new Point(86, 137);
+            c.Name = "c";
+            c.Size = new Size(55, 45);
+            c.TabIndex = 22;
+            c.Text = "C";
+            c.UseVisualStyleBackColor = true;
+            c.Click += c_Click;
+            // 
+            // ce
+            // 
+            ce.Location = new Point(12, 137);
+            ce.Name = "ce";
+            ce.Size = new Size(55, 45);
+            ce.TabIndex = 21;
+            ce.Text = "CE";
+            ce.UseVisualStyleBackColor = true;
+            ce.Click += ce_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 64, 64);
+            ClientSize = new Size(307, 488);
+            Controls.Add(faiz);
+            Controls.Add(sqrt);
+            Controls.Add(c);
+            Controls.Add(ce);
+            Controls.Add(nisbet);
+            Controls.Add(beraber);
+            Controls.Add(r0);
+            Controls.Add(nokta);
+            Controls.Add(hasil);
+            Controls.Add(r9);
+            Controls.Add(r8);
+            Controls.Add(r7);
+            Controls.Add(ferq);
+            Controls.Add(r6);
+            Controls.Add(r5);
+            Controls.Add(r4);
+            Controls.Add(cem);
+            Controls.Add(r3);
+            Controls.Add(r2);
+            Controls.Add(r1);
+            Controls.Add(richTextBox1);
+            Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calcultor";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button r1;
+        private Button r2;
+        private Button r3;
+        private Button cem;
+        private Button ferq;
+        private Button r6;
+        private Button r5;
+        private Button r4;
+        private Button hasil;
+        private Button r9;
+        private Button r8;
+        private Button r7;
+        private Button nisbet;
+        private Button beraber;
+        private Button r0;
+        private Button nokta;
+        private Button faiz;
+        private Button sqrt;
+        private Button c;
+        private Button ce;
+    }
+}
